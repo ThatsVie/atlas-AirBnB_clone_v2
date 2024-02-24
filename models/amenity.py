8fb3155c142bb33a7ba41bb989f5fe80f9ee7776
Srixx24 <144152489+Srixx24@users.noreply.github.com>
2024-02-24 09:58:31 -0600

    changing import

--- models/amenity.py
+++ models/amenity.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
 from models.base_model import BaseModel
-from models.place import place_amenity_table
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 
