0926bb64755c562599458882be5d930f1ee3f46f
Vie <143755961+ThatsVie@users.noreply.github.com>
2024-02-25 18:04:08 -0600

    last of tests for today

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -1,109 +1,212 @@
 #!/usr/bin/python3
-""" Module for testing file storage"""
-import unittest
-from models.base_model import BaseModel
-from models import storage
+""" Tests for file storage """
 import os
+import unittest
+from models import storage
+from genericpath import exists
+from models.base_model import BaseModel
+from models.engine.file_storage import FileStorage
 
 
-class test_fileStorage(unittest.TestCase):
-    """ Class to test the file storage method """
+@unittest.skipIf(os.getenv("HBNB_ENV") != "file", 'FileStorage inactive')
+class TestFileStorageInitialization(unittest.TestCase):
+    """ Tests for FileStorage class initialization """
 
-    def setUp(self):
-        """ Set up test environment """
-        del_list = []
-        for key in storage._FileStorage__objects.keys():
-            del_list.append(key)
-        for key in del_list:
-            del storage._FileStorage__objects[key]
+    def test_documentation(self):
+        """ Ensures all necessary docstrings are present """
+        self.assertTrue(len(FileStorage.__doc__) > 0)
+        self.assertTrue(len(FileStorage.all.__doc__) > 0)
+        self.assertTrue(len(FileStorage.new.__doc__) > 0)
+        self.assertTrue(len(FileStorage.save.__doc__) > 0)
+        self.assertTrue(len(FileStorage.reload.__doc__) > 0)
+        self.assertTrue(len(FileStorage.delete.__doc__) > 0)
 
-    def tearDown(self):
-        """ Remove storage file at end of tests """
+    def test_code_style(self):
+        """ Checks code style compliance with PEP 8 """
+        style = pycodestyle.StyleGuide(quiet=True)
+        self.assertEqual(
+            style.check_files(['models/engine/file_storage.py']).total_errors,
+            0,
+            "Found code style errors (and warnings)."
+        )
+
+    def test_invalid_argument(self):
+        """ Verifies TypeError is raised when an argument is supplied """
+        with self.assertRaises(TypeError):
+            FileStorage(1)
+
+    def test_private_attributes(self):
+        """ Tests types and privacy of class attributes """
+        self.assertTrue(type(storage._FileStorage__file_path) is str)
+        self.assertTrue(type(storage._FileStorage__objects) is dict)
+        with self.assertRaises(AttributeError):
+            print(storage.__objects)
+            print(storage.__file_path)
+
+
+@unittest.skipIf(os.getenv("HBNB_ENV") != "file", 'FileStorage inactive')
+class TestFileStorageAllMethod(unittest.TestCase):
+    """ Tests for FileStorage all method """
+
+    @classmethod
+    def setUpClass(cls):
+        """ Sets up objects for testing """
+        cls.obj = BaseModel()
+        cls.obj.save()
+
+    @classmethod
+    def tearDownClass(cls):
+        """ Cleans up after testing """
+        del cls.obj
         try:
-            os.remove('file.json')
-        except:
+            os.remove("file.json")
+        except IOError:
             pass
-
-    def test_obj_list_empty(self):
-        """ __objects is initially empty """
-        self.assertEqual(len(storage.all()), 0)
-
-    def test_new(self):
-        """ New object is correctly added to __objects """
-        new = BaseModel()
-        for obj in storage.all().values():
-            temp = obj
-        self.assertTrue(temp is obj)
 
     def test_all(self):
-        """ __objects is properly returned """
-        new = BaseModel()
-        temp = storage.all()
-        self.assertIsInstance(temp, dict)
+        """ Checks correct operation of all method """
+        self.assertTrue(self.obj, exists)
+        self.assertIn(
+            f'{self.obj.__class__.__name__}.{self.obj.id}',
+            storage.all()
+        )
+        self.assertTrue(type(storage.all()) is dict)
+        self.assertTrue(len(storage.all()) > 0)
 
-    def test_base_model_instantiation(self):
-        """ File is not created on BaseModel save """
-        new = BaseModel()
-        self.assertFalse(os.path.exists('file.json'))
 
-    def test_empty(self):
-        """ Data is saved to file """
-        new = BaseModel()
-        thing = new.to_dict()
-        new.save()
-        new2 = BaseModel(**thing)
-        self.assertNotEqual(os.path.getsize('file.json'), 0)
+@unittest.skipIf(os.getenv("HBNB_ENV") != "file", 'FileStorage inactive')
+class TestFileStorageNewMethod(unittest.TestCase):
+    """ Tests for FileStorage new method """
+
+    @classmethod
+    def setUpClass(cls):
+        """ Sets up objects for testing """
+        cls.obj = BaseModel()
+        cls.obj.save()
+
+    @classmethod
+    def tearDownClass(cls):
+        """ Cleans up after testing """
+        del cls.obj
+        try:
+            os.remove("file.json")
+        except IOError:
+            pass
+
+    def test_new(self):
+        """ Checks correct operation of new method """
+        storage.new(self.obj)
+        self.assertIn(
+            f'{self.obj.__class__.__name__}.{self.obj.id}',
+            storage._FileStorage__objects
+        )
+        self.assertIn(self.obj, storage.all().values())
+
+    def test_invalid_arguments(self):
+        """ Verifies exceptions are raised with invalid arguments """
+        with self.assertRaises(TypeError):
+            storage.new(self.obj, 6)
+        with self.assertRaises(AttributeError):
+            storage.new(8)
+            storage.new({2, 4, 8})
+
+
+@unittest.skipIf(os.getenv("HBNB_ENV") != "file", 'FileStorage inactive')
+class TestFileStorageSaveMethod(unittest.TestCase):
+    """ Tests for FileStorage save method """
+
+    @classmethod
+    def setUpClass(cls):
+        """ Sets up objects for testing """
+        cls.obj = BaseModel()
+        cls.obj.save()
+
+    @classmethod
+    def tearDownClass(cls):
+        """ Cleans up after testing """
+        del cls.obj
+        try:
+            os.remove("file.json")
+        except IOError:
+            pass
 
     def test_save(self):
-        """ FileStorage save method """
-        new = BaseModel()
-        storage.save()
+        """ Checks correct operation of save method """
+        self.obj.save()
         self.assertTrue(os.path.exists('file.json'))
+        with open("file.json") as file:
+            content = file.read()
+            self.assertIn(
+                f"{self.obj.__class__.__name__}.{self.obj.id}",
+                content
+            )
+            self.assertTrue(len(content) > 0)
+
+    def test_invalid_arguments(self):
+        """ Verifies exceptions are raised with invalid arguments """
+        with self.assertRaises(TypeError):
+            storage.save(1)
+            self.obj.save(1)
+
+
+@unittest.skipIf(os.getenv("HBNB_ENV") != "file", 'FileStorage inactive')
+class TestFileStorageReloadMethod(unittest.TestCase):
+    """ Tests for FileStorage reload method """
 
     def test_reload(self):
-        """ Storage file is successfully loaded to __objects """
-        new = BaseModel()
-        storage.save()
+        """ Checks correct operation of reload method """
+        obj = BaseModel()
+        obj.save()
         storage.reload()
-        for obj in storage.all().values():
-            loaded = obj
-        self.assertEqual(new.to_dict()['id'], loaded.to_dict()['id'])
-
-    def test_reload_empty(self):
-        """ Load from an empty file """
-        with open('file.json', 'w') as f:
+        self.assertIn(
+            f'{obj.__class__.__name__}.{obj.id}',
+            storage._FileStorage__objects
+        )
+        self.assertTrue(len(storage.all()) > 0)
+        del obj
+        try:
+            os.remove("file.json")
+        except IOError:
             pass
-        with self.assertRaises(ValueError):
-            storage.reload()
 
-    def test_reload_from_nonexistent(self):
-        """ Nothing happens if file does not exist """
-        self.assertEqual(storage.reload(), None)
+    def test_invalid_arguments(self):
+        """ Verifies exceptions are raised with invalid arguments """
+        with self.assertRaises(TypeError):
+            storage.reload(1)
 
-    def test_base_model_save(self):
-        """ BaseModel save method calls storage save """
-        new = BaseModel()
-        new.save()
-        self.assertTrue(os.path.exists('file.json'))
 
-    def test_type_path(self):
-        """ Confirm __file_path is string """
-        self.assertEqual(type(storage._FileStorage__file_path), str)
+@unittest.skipIf(os.getenv("HBNB_ENV") != "file", 'FileStorage inactive')
+class TestFileStorageDeleteMethod(unittest.TestCase):
+    """ Tests for FileStorage delete method """
 
-    def test_type_objects(self):
-        """ Confirm __objects is a dict """
-        self.assertEqual(type(storage.all()), dict)
+    @classmethod
+    def setUpClass(cls):
+        """ Sets up objects for testing """
+        cls.obj = BaseModel()
+        cls.obj.save()
+        cls.obj_to_delete = BaseModel()
+        cls.obj_to_delete.save()
 
-    def test_key_format(self):
-        """ Key is properly formatted """
-        new = BaseModel()
-        _id = new.to_dict()['id']
-        for key in storage.all().keys():
-            temp = key
-        self.assertEqual(temp, 'BaseModel' + '.' + _id)
+    @classmethod
+    def tearDownClass(cls):
+        """ Cleans up after testing """
+        del cls.obj
+        del cls.obj_to_delete
+        try:
+            os.remove("file.json")
+        except IOError:
+            pass
 
-    def test_storage_var_created(self):
-        """ FileStorage object storage created """
-        from models.engine.file_storage import FileStorage
-        print(type(storage))
-        self.assertEqual(type(storage), FileStorage)
+    def test_delete(self):
+        """ Checks correct operation of delete method """
+        initial_state = storage.all().copy()
+        obj_id = f'{self.obj_to_delete.__class__.__name__}.{self.obj_to_delete.id}'
+        self.assertEqual(initial_state, storage.all())
+        self.assertIn(obj_id, storage.all())
+        storage.delete(self.obj_to_delete)
+        self.assertNotEqual(initial_state, storage.all())
+        self.assertNotIn(obj_id, storage.all())
+
+
+if __name__ == "__main__":
+    unittest.main()
