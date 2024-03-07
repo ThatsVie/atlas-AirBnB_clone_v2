24a06677d24c921856093b1ab86fdb403951b41d
Vie <143755961+ThatsVie@users.noreply.github.com>
2024-03-07 15:23:14 -0600

    5

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -1,1 +1,72 @@
 #!/usr/bin/python3
+"""
+Starts a Flask web application listening on port 5000.
+Defines a Flask application with five routes:
+- "/" displays "Hello HBNB!"
+- "/hbnb" displays "HBNB"
+- "/c/<text>" displays "C " followed by the value of the
+text variable, replacing underscores with spaces.
+- "/python/<text>" displays "Python ",
+followed by the value of the text variable
+- "/number/<n>" displays "<n> is a number" only if n is an integer
+"""
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """
+    Displays "Hello HBNB!" when accessing root route ("/").
+    """
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """
+    Displays "HBNB" when accessing the "/hbnb" route.
+    """
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_route(text):
+    """
+    Displays "C " followed by the value of the text variable,
+    replacing underscores with spaces.
+
+    Args:
+    - text: text variable from the URL path.
+    """
+    return 'C {}'.format(text.replace('_', ' '))
+
+
+@app.route('/python', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python_route(text='is cool'):
+    """
+    Displays "Python ", followed by the value of the text variable,
+    replacing underscores with spaces.
+
+    Args:
+    - text: text variable from the URL path.
+    - default value of text is "is cool"
+    """
+    return 'Python {}'.format(text.replace('_', ' '))
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def number_route(n):
+    """
+    Displays "<n> is a number" only if n is an integer.
+
+    Args:
+    - n: integer variable from the URL path.
+    """
+    return '{} is a number'.format(n)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
