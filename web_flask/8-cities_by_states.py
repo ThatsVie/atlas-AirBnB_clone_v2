934781bdb880cb732db865decffa85c06f114a54
ThatsVie <Viepaulalmt@gmail.com>
2024-03-08 14:08:30 -0800

    cities by states

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -19,6 +19,12 @@ UL tag: with the list of all State objects present in DBStorage
 sorted by name (A->Z)
 LI tag: description of one State: <state.id>: <B><state.name></B>
 - /cities_by_states: display a HTML page: (inside the tag BODY)
+H1 tag: “States”
+UL tag: with the list of all State objects present in DBStorage
+sorted by name (A->Z)
+LI tag: description of one State: <state.id>: <B><state.name></B> + 
+UL tag: with the list of City objects linked to the State sorted by name (A->Z)
+LI tag: description of one City: <city.id>: <B><city.name></B>
 """
 from flask import Flask
 from flask import render_template
@@ -113,6 +119,16 @@ def state_list():
     """
     states = storage.all('State')
     return render_template('7-states_list.html', states=states)
+
+
+@app.route('/cities_by_states', strict_slashes=False)
+def cities_by_states():
+    """
+    Display a list of states and their associated cities
+    """
+    states = storage.all('State')
+    return render_template('8-cities_by_states.html', states=states)
+
 
 
 @app.teardown_appcontext
