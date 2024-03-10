378a70eee30a7df8e3caa7b4aab83250f46da1e2
Vie <143755961+ThatsVie@users.noreply.github.com>
2024-03-10 16:21:29 -0500

    appending the root directory to the Python system path before import models

--- web_flask/10-hbnb_filters.py
+++ web_flask/10-hbnb_filters.py
@@ -4,6 +4,10 @@ Starts a Flask web application
 Listens to host='0.0.0.0', port=5000
 - /hbnb_filters: display a HTML page
 """
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+
 from flask import Flask, render_template
 from models import storage
 
