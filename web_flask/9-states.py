7c3adaf720458363d7971199f5c38ec81148fd18
Vie <143755961+ThatsVie@users.noreply.github.com>
2024-03-12 13:27:00 -0500

    9-states.py

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -137,10 +137,12 @@ def states_id(id):
     """
     Display cities with state id
     """
-    for state in storage.all('State').values():
-        if state.id == id:
-            return render_template('9-states.html', state=state)
+    state = storage.get('State', id)
+    if state:
+        return render_template('9-states.html', state=state)
     return render_template('9-states.html')
+
+    
 
 
 @app.teardown_appcontext
