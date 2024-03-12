c1d9f79aa81187e4356e20764c67bdd290b0f21c
ThatsVie <Viepaulalmt@gmail.com>
2024-03-12 09:37:34 -0700

    pep8

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -5,7 +5,7 @@ Starts a Flask web application listening on port 5000.
 H1 tag: “States”
 UL tag: with the list of all State objects present in DBStorage
 sorted by name (A->Z)
-LI tag: description of one State: <state.id>: <B><state.name></B> + 
+LI tag: description of one State: <state.id>: <B><state.name></B> +
 UL tag: with the list of City objects linked to the State sorted by name
 LI tag: description of one City: <city.id>: <B><city.name></B>
 """
@@ -41,4 +41,3 @@ def teardown(exception):
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
-
