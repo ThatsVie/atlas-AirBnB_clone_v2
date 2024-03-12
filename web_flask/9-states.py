8f20dd4b437db9aecaa6921385ea492ac6914315
Vie <143755961+ThatsVie@users.noreply.github.com>
2024-03-12 15:16:04 -0500

    9-states.py

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 """
 Starts a Flask web application listening on port 5000.
 Defines a Flask application with eleven routes:
