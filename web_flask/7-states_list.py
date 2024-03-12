0835460b0b701e0e29bd4a1cd3d260a51888b004
Vie <143755961+ThatsVie@users.noreply.github.com>
2024-03-12 14:44:27 -0500

    7-states_list.py

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -3,12 +3,10 @@
 Starts a Flask web application listening on port 5000.
 - /states_list displays a list of all State objects sorted by name
 """
-import sys
-import os
-
 from flask import Flask
 from flask import render_template
 from models import storage
+
 
 app = Flask(__name__)
 
@@ -18,7 +16,7 @@ def state_list():
     """
     Displays a list of states.
     """
-    states = storage.all('State')
+    states = storage.all('State').values()
     return render_template('7-states_list.html', states=states)
 
 
