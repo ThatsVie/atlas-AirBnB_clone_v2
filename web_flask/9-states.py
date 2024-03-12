5342ea04284dfd3a6634d0f969fa3bf6e39deda1
Vie <143755961+ThatsVie@users.noreply.github.com>
2024-03-11 21:23:09 -0500

    9-states.py

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -111,7 +111,7 @@ def state_list():
     """
     Displays a list of states.
     """
-    states = storage.all('State')
+    states = storage.all(State)
     return render_template('7-states_list.html', states=states)
 
 
@@ -120,7 +120,7 @@ def cities_by_states():
     """
     Display a list of states and their associated cities
     """
-    states = storage.all('State')
+    states = storage.all(State)
     return render_template('8-cities_by_states.html', states=states)
 
 
@@ -129,7 +129,7 @@ def states():
     """
     Display a list of all State objects
     """
-    states = storage.all('State')
+    states = storage.all(State)
     return render_template('8-cities_by_states.html', states=states)
 
 
